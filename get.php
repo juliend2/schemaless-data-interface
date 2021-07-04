2eed4633ddf87f52842f4f5260412f9e4cd9cb2d
Julien Desrosiers <juliend2@gmail.com>
2021-07-03 23:23:43 -0400

    comments

--- get.php
+++ get.php
@@ -6,9 +6,16 @@ header('Content-Type: application/json;charset=utf-8');
 
 // Is Asking for a JSONP answer?
 if (isset($_GET['callback']) && $_GET['callback'] != '') {
+
+    // Example:
+    // http://localhost/schemaless/get.php?namespace=bobjoie&callback=func
     echo $_GET['callback']."(". json_encode($data) . ')';
+    
 } else {
     // Only JSON:
+
+    // Example:
+    // http://localhost/schemaless/get.php?namespace=bobjoie
     echo json_encode($data);
 }
 
