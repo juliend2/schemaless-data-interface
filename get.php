fa98902cfa92966a22e587db60c289e96fe1638a
Julien Desrosiers <juliend2@gmail.com>
2021-07-03 23:20:40 -0400

    support JSONP

--- get.php
+++ get.php
@@ -2,6 +2,14 @@
 include 'db.php';
 $records = get_all_by_namespace($pdo, $_GET['namespace']);
 $data = $records;
-header('Content-Type: application/json');
-echo json_encode($data);
+header('Content-Type: application/json;charset=utf-8');
+
+// Is Asking for a JSONP answer?
+if (isset($_GET['callback']) && $_GET['callback'] != '') {
+    echo $_GET['callback']."(". json_encode($data) . ')';
+} else {
+    // Only JSON:
+    echo json_encode($data);
+}
+
 die;
